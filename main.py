39a78e5a288332c00633ba665d7da74cb33bc289
qwality <low.qwality@gmail.com>
2023-11-06 10:37:19 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory='templates')
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get('/')
+async def index(request: Request):
+    return templates.TemplateResponse('main_cv.html', {'request':request})
