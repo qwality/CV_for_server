2bf5de92beccb7665a47e22718eeac1d05ae2167
qwality <low.qwality@gmail.com>
2023-11-11 12:53:33 +0100

    redeploy bez .sh

--- scripts/set_redeploy.sh
+++ scripts/set_redeploy.sh
@@ -3,12 +3,12 @@
 # Nastavit absolutní cestu k složce skriptu
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-# Nastavit redeploy.sh jako spustitelný
-chmod +x "$SCRIPT_DIR/redeploy.sh"
+# Nastavit redeploy jako spustitelný
+chmod +x "$SCRIPT_DIR/redeploy"
 
 # Přidat cestu do proměnné PATH, pokud již není přidána
 if ! grep -q "$SCRIPT_DIR" ~/.bashrc; then
-    echo 'export PATH="$PATH:'"$SCRIPT_DIR/redeploy.sh"'"' >> ~/.bashrc
+    echo 'export PATH="$PATH:'"$SCRIPT_DIR/redeploy"'"' >> ~/.bashrc
 fi
 
 # Aplikovat změny
