0968c8c775a8ad4db4854155ec59553f2d946e46
qwality <low.qwality@gmail.com>
2023-11-11 12:14:10 +0100

    relativni cesta

--- scripts/test_cert.sh
+++ scripts/test_cert.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-echo "$1" > /home/cv/static/.well-known/acme-challenge/test-file.txt
+# Nastavíme pracovní adresář na adresář, ve kterém je umístěn skript
+cd "$(dirname "$0")"
+
+# Změníme cestu na relativní, předpokládáme, že složka .well-known je ve složce /home/cv/static
+echo "$1" > ../static/.well-known/acme-challenge/test-file.txt
 curl http://www.qwality.fun/.well-known/acme-challenge/test-file.txt
