db91c85c9e6bdd22668011e4fe78ef19211672c0
qwality <low.qwality@gmail.com>
2023-11-11 13:43:01 +0100

    80

--- scripts/_run_uvicorn.sh
+++ scripts/_run_uvicorn.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# uvicorn 'main:app' --host '0.0.0.0' --port '80' &
+uvicorn 'main:app' --host '0.0.0.0' --port '80' &
 
 APP='main:app'
 HOST='0.0.0.0'
@@ -7,4 +7,4 @@ PORT='443'
 CERT='/etc/letsencrypt/live/qwality.fun/cert.pem'
 KEY='/etc/letsencrypt/live/qwality.fun/privkey.pem'
 
-uvicorn $APP --host $HOST --port $PORT --ssl-keyfile $CERT --ssl-certfile $KEY
+# uvicorn $APP --host $HOST --port $PORT --ssl-keyfile $CERT --ssl-certfile $KEY
