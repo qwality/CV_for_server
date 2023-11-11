427da3de4f10c8309bb858fe53523b46fd82ac17
qwality <low.qwality@gmail.com>
2023-11-11 13:39:35 +0100

    cert

--- scripts/_run_uvicorn.sh
+++ scripts/_run_uvicorn.sh
@@ -4,7 +4,7 @@
 APP='main:app'
 HOST='0.0.0.0'
 PORT='443'
-CERT='/etc/letsencrypt/live/qwality.fun/fullchain.pem'
+CERT='/etc/letsencrypt/live/qwality.fun/cert.pem'
 KEY='/etc/letsencrypt/live/qwality.fun/privkey.pem'
 
 uvicorn $APP --host $HOST --port $PORT --ssl-keyfile $CERT --ssl-certfile $KEY
