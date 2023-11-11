6cc2763ce70b883a94e9a5c06bd484834de12c05
qwality <low.qwality@gmail.com>
2023-11-11 13:03:47 +0100

    fewre

--- scripts/set_redeploy.sh
+++ scripts/set_redeploy.sh
@@ -7,7 +7,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 chmod +x "$SCRIPT_DIR/redeploy"
 
 # Přidat cestu do proměnné PATH, pokud již není přidána
-if ! grep -q "$SCRIPT_DIR" ~/.bashrc; then
+if ! grep -q "$SCRIPT_DIR/redeploy" ~/.bashrc; then
     echo 'export PATH="$PATH:'"$SCRIPT_DIR/redeploy"'"' >> ~/.bashrc
 fi
 
